6f4cb4f3fdb2800921dd223562d0f352f6b00160
Husain Barwaniwala <61308223+Husain3043@users.noreply.github.com>
2022-08-10 18:11:30 +0530

    Coding challenge 2

--- coding_challenge_1.js
+++ coding_challenge_1.js
@@ -9,3 +9,10 @@ const johnBMI = johnWeight / (johnWeight ** 2);
 
 const markHigherBMI = markBMI > johnBMI;
 console.log("Does mark has higher BMI? " + markHigherBMI);
+
+// Coding challenge 2:
+if (markHigherBMI) {
+    console.log(`Mark has higher BMI (${markBMI}) than John (${johnBMI})`);
+} else {
+    console.log(`John has higher BMI (${johnBMI}) than Mark (${markBMI})`);
+}
