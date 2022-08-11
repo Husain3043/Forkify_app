71506f486cd12375e9ea33aa170d516c17367d44
Husain Barwaniwala <61308223+Husain3043@users.noreply.github.com>
2022-08-11 19:06:30 +0530

    Coding challenge 4

--- coding_challenges.js
+++ coding_challenges.js
@@ -29,3 +29,15 @@ if (dolphinScoreAvg > koalasScoreAvg) {
 } else {
     console.log("Competition tied!!");
 }
+
+//Coding challenge 4:
+const bill1 = 275;
+const bill2 = 40;
+const bill3 = 430;
+
+const tipPercent1 = bill1 > 50 && bill1 < 300 ? 0.15 : 0.2;
+const tipPercent2 = bill2 > 50 && bill2 < 300 ? 0.15 : 0.2;
+const tipPercent3 = bill3 > 50 && bill3 < 300 ? 0.15 : 0.2;
+console.log(`The bill 1 was ${bill1}, the tip 1 was ${tipPercent1 * bill1} and the total value 1 ${bill1 + tipPercent1 * bill1}.`)
+console.log(`The bill 2 was ${bill2}, the tip 2 was ${tipPercent2 * bill2} and the total value 2 ${bill2 + tipPercent2 * bill2}.`)
+console.log(`The bill 3 was ${bill3}, the tip 3 was ${tipPercent3 * bill3} and the total value 3 ${bill3 + tipPercent3 * bill3}.`)
